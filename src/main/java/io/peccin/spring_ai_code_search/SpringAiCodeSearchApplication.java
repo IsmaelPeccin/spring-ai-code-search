c819b67eec963c1203bfd7bdc08ee37824bb4394
IsmaelPeccin <ismaelpeccin@gmail.com>
2026-05-11 22:18:20 -0300

    feat: init project

---
+++ src/main/java/io/peccin/spring_ai_code_search/SpringAiCodeSearchApplication.java
@@ -0,0 +1,13 @@
+package io.peccin.spring_ai_code_search;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAiCodeSearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAiCodeSearchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/peccin/spring_ai_code_search/SpringAiCodeSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package io.peccin.spring_ai_code_search;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAiCodeSearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
